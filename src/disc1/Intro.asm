7aacdfbe74600f73977b9fa768d58e4f40c3a7fe
RadiantHero6 <90285213+RadiantHero6@users.noreply.github.com>
2023-07-10 23:31:12 -0400

    Update Intro.asm
    New translation for the intro, tested to not crash, don't believe any typos.

--- src/disc1/Intro.asm
+++ src/disc1/Intro.asm
@@ -1,107 +1,107 @@
 // Introduction
 TextMain($43A45538, 0, 1, "Jigen]")
-TextMain($43A4554E, 0, 0, "I was early ")
+TextMain($43A4554E, 0, 0, "Thats quick.")
 
 TextMain($43A455B2, 0, 1, "Jigen]")
-TextMain($43A455C8, 0, 0, "Did it go well? ")
+TextMain($43A455C8, 1, 0, "Did it go well?")
 
 TextMain($43A45600, 12, 1, "Lupin]  ")
-TextMain($43A45624, 16, 0, "Perfect!    ")
+TextMain($43A45624, 18, 0, "Perfectly!")
 
 TextMain($43A45668, 12, 1, "Lupin]  ")
-TextMain($43A4568C, 16, 0, "Look!!  ")
+TextMain($43A4568C, 18, 0, "Look!!")
 
 TextMain($43A456C8, 4, 1, "Jigen]        ")
-TextMain($43A456EA, 8, 0, "Huh...        ")
-TextMain($43A45706, 8, 0, "What on Earth is this?")
-
+TextMain($43A456EA, 13, 0, "Oh, no...")
+TextMain($43A45706, 8, 0, "What the hell is this?")
+                        
 TextMain($43A4574C, 0, 1, "Jigen]")
-TextMain($43A45762, 2, 0, "Hey, Lupin!   ")
-TextMain($43A45778, 2, 0, "All that for this?")
+TextMain($43A45762, 4, 0, "Wait, Lupin!")
+TextMain($43A45778, 3, 0, "We came for this?")
 
 TextMain($43A457B4, 8, 1, "Lupin]    ")
-TextMain($43A457D6, 8, 0, "Yes, just for that! ")
+TextMain($43A457D6, 9, 0, "Yes, just for that.")
 
 TextMain($43A4581A, 0, 1, "Jigen]")
-TextMain($43A45830, 0, 0, "O.K then")
-TextMain($43A4583E, 0, 0, "as we're in Greece let's have a drink!")
+TextMain($43A45830, 0, 0, "Really? ")
+TextMain($43A4583E, 0, 0, "We came to Greece just for this thing?")
 
 TextMain($43A4588C, 8, 1, "Lupin]    ")
-TextMain($43A458AE, 8, 0, "However, that person")
-TextMain($43A458D0, 8, 0, "depending on the \dtreasure\d!")
+TextMain($43A458AE, 12, 0, "But this 'thing'")
+TextMain($43A458D0, 16, 0, "Is a great treasure!")
 
 TextMain($43A4591C, 0, 1, "Jigen]")
-TextMain($43A45932, 2, 0, "This is?")
+TextMain($43A45932, 2, 0, "Really? ")
 
 TextMain($43A45964, 10, 1, "Jigen]")
-TextMain($43A45984, 10, 0, "Also, this is very fine   ")
-TextMain($43A459AE, 10, 0, "do you not see...   ")
+TextMain($43A45984, 10, 0, "For such an imposing thing")
+TextMain($43A459AE, 11, 0, "I just dont see it.")
 
 TextMain($43A459F4, 4, 1, "Jigen]")
-TextMain($43A45A0E, 4, 0, "What is it?     ")
+TextMain($43A45A0E, 4, 0, "So what is this?")
 
 TextMain($43A45A4A, 12, 1, "Lupin]        ")
-TextMain($43A45A74, 12, 0, "It might be a key...")
+TextMain($43A45A74, 24, 0, "Kirim...")
 
 TextMain($43A45ABC, 4, 1, "Jigen]")
-TextMain($43A45AD6, 4, 0, "A key?  ")
+TextMain($43A45AD6, 6, 0, "Kirim?")
 
 TextMain($43A45B2A, 2, 1, "Lupin]  ")
-TextMain($43A45B44, 2, 0, "Yep, what is that!? ")
+TextMain($43A45B44, 7, 0, "What the hell?!")
 
 TextMain($43A45BA2, 18, 1, "Jigen]")
-TextMain($43A45BCA, 16, 0, "What's happening!?  ")
+TextMain($43A45BCA, 18, 0, "What's happening?!")
 
 TextMain($43A45C56, 14, 1, "Jigen]")
-TextMain($43A45C7A, 18, 0, "Aaargh! ")
+TextMain($43A45C7A, 20, 0, "Whoa!!")
 
 TextMain($43A45CBC, 0, 1, "Lupin]  ")
-TextMain($43A45CD4, 0, 0, "Jigen, you got a new friend!")
+TextMain($43A45CD4, 0, 0, "Jigen, you got a new friend?")
 
 TextMain($43A45D18, 18, 1, "Jigen]")
-TextMain($43A45D40, 21, 0, "I do not know")
-TextMain($43A45D68, 21, 0, "this guy!!   ")
+TextMain($43A45D40, 22, 0, "I don't know")
+TextMain($43A45D68, 25, 0, "this guy!")
 
 TextMain($43A45DB2, 0, 1, "Lupin]  ")
 TextMain($43A45DCA, 0, 0, "Jigen!")
-TextMain($43A45DD6, 0, 0, "He is trying to kill you! ")
+TextMain($43A45DD6, 7, 0, "He wants the Kirim!")
 
 TextMain($43A45E18, 14, 1, "Jigen]")
-TextMain($43A45E3C, 18, 0, "What?   ")
+TextMain($43A45E3C, 21, 0, "What?")
 
 TextMain($43A45E7E, 2, 1, "Jigen]")
-TextMain($43A45E96, 1, 0, "That guy, made a hole in my gun barrel!")
+TextMain($43A45E96, 2, 0, "That bastard put a hole in the barrel!")
 
 TextMain($43A45FB2, 6, 1, "Jigen]")
-TextMain($43A45FED, 0, 0, "Apparently in addition to us, it seems")
-TextMain($43A46019, 0, 0, "there are guys also looking for this. ")
+TextMain($43A45FED, 0, 0, "Looks like there's more people than us")
+TextMain($43A46019, 7, 0, "that's after this thing, Lupin.")
 
 TextMain($43A46095, 12, 1, "Lupin]  ")
-TextMain($43A460E6, 1, 0, "Do you think so?           ")
-TextMain($43A46108, 1, 0, "That guy, he was really trying to get it!")
+TextMain($43A460E6, 13, 0, "What did I say?")
+TextMain($43A46108, 8, 0, "I told you it is a great treasure!")
 
 TextMain($43A4618C, 6, 1, "Jigen]")
-TextMain($43A461B0, 0, 0, "... Also,   ")
-TextMain($43A461DF, 0, 0, "how was he so incredible? ")
+TextMain($43A461B0, 7, 0, "So...")
+TextMain($43A461DF, 1, 0, "what's so great about it?")
 
 TextMain($43A46269, 12, 1, "Lupin]  ")
-TextMain($43A462AC, 10, 0, "I bet we are going to meet more ")
-TextMain($43A462DC, 10, 0, "incredible people like that!")
+TextMain($43A462AC, 10, 0, "Im going to somone who knows why")
+TextMain($43A462DC, 14, 0, "we're going to them now!")
 
 TextMain($43A46372, 6, 1, "Jigen]")
-TextMain($43A463A5, 0, 0, "What? You   ")
-TextMain($43A463B7, 0, 0, "do not know that for sure!")
+TextMain($43A463A5, 1, 0, "Wait, what?")
+TextMain($43A463B7, 5, 0, "You don't even know?!")
 
 TextMain($43A4642B, 12, 1, "Lupin]  ")
-TextMain($43A46466, 12, 0, "Well  ")
-TextMain($43A4647E, 12, 0, "we will have to see!")
+TextMain($43A46466, 12, 0, "No but")
+TextMain($43A4647E, 11, 0, "we'll learn together.")
 
 TextMain($43A464F8, 6, 1, "Jigen]")
-TextMain($43A46533, 6, 0, "O.K...      ")
+TextMain($43A46533, 6, 0, "Seriously...")
 
 TextMain($43A46611, 10, 1, "Zenigata] ")
 TextMain($43A46635, 0, 0, "He was 1 step ahead of me,")
-TextMain($43A46655, 0, 0, "I got here too late to catch him!       ")
+TextMain($43A46655, 7, 0, "I got here too late to catch him!")
 
 TextMain($43A466A5, 8, 1, "Zenigata] ")
 TextMain($43A466C7, 8, 0, "I will find you Lupin!")
