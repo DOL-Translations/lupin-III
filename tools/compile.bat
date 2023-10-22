bc4ddfcef00adbbb7806ee0bf295347b2a610972
NewGBAXL <81663474+NewGBAXL@users.noreply.github.com>
2023-10-22 16:31:24 -0400

    System Text & BinString.exe
    -translated System text
    -added BinString.exe to handle easy text insertion
    -updated opening.bnr

--- tools/compile.bat
+++ tools/compile.bat
@@ -1,4 +1,17 @@
 @echo off
+
+set ISOFile=..\input\Lupin Disc 1 [J].iso
+
+if not exist "%ISOFile%" (
+	echo [INFO] "%ISOFile%" was not found
+	echo Did you name the ROM correctly and place it in the correct folder?
+	echo Exiting in 10 seconds..
+	timeout /t 10 /nobreak >nul
+	exit /b 0
+)
+
+BinString.exe patch --source C:/Users/newgb/Documents/GitHub/DOL-Translations/lupin-iii/src/disc1/fs/COMMON/ --filter sfil.bin --output C:/Users/newgb/Documents/GitHub/DOL-Translations/lupin-iii/src/disc1/fs/COMMON/ --patch C:/Users/newgb/Documents/GitHub/DOL-Translations/lupin-iii/src/disc1/fs/COMMON/ --encoding "shift jis" --verbose
+
 echo [INFO] Compiling patches for disc1
 
 bass\\win\\bass.exe ..\\src\\disc1\\Main.asm
