bc5f73eed2d2b8c57cf219ac90da59102a68455a
NewGBAXL <81663474+NewGBAXL@users.noreply.github.com>
2023-12-29 13:47:12 -0500

    Setup for Disc 2

---
+++ src/disc2/Dialogue.asm
@@ -0,0 +1,2 @@
+// Dialogue
+ReplaceAsset($417D64D8, "../common/fs/COMMON/sfil.BIN", 667392)
--- tools/compile.bat
+++ tools/compile.bat
@@ -1,22 +1,31 @@
 @echo off
 
-set ISOFile=..\input\Lupin Disc 1 [J].iso
+set ISOFile1=..\input\Lupin Disc 1 [J].iso
+set ISOFile2=..\input\Lupin Disc 2 [J].iso
 
-if not exist "%ISOFile%" (
-	echo [INFO] "%ISOFile%" was not found
+if not exist "%ISOFile1%" (
+	echo [INFO] "%ISOFile1%" was not found
 	echo Did you name the ROM correctly and place it in the correct folder?
 	echo Exiting in 10 seconds..
-	timeout /t 10 /nobreak >nul
+	C:\Windows\System32\timeout.exe /t 10 /nobreak >nul
 	exit /b 0
 )
 
-BinString.exe patch --source C:/Users/newgb/Documents/GitHub/DOL-Translations/lupin-iii/src/disc1/fs/COMMON/ --filter sfil.bin --output C:/Users/newgb/Documents/GitHub/DOL-Translations/lupin-iii/src/disc1/fs/COMMON/ --patch C:/Users/newgb/Documents/GitHub/DOL-Translations/lupin-iii/src/disc1/fs/COMMON/ --encoding "shift jis" --verbose
+BinString.exe patch --source C:/Users/newgb/Documents/GitHub/DOL-Translations/lupin-iii/src/common/fs/COMMON/ --filter sfil.bin --output C:/Users/newgb/Documents/GitHub/DOL-Translations/lupin-iii/src/common/fs/COMMON/ --patch C:/Users/newgb/Documents/GitHub/DOL-Translations/lupin-iii/src/common/fs/COMMON/ --encoding "shift jis" --verbose
 
 echo [INFO] Compiling patches for disc1
 
 bass\\win\\bass.exe ..\\src\\disc1\\Main.asm
 
 echo [INFO] Patches compiled for disc1
+
+if not exist "%ISOFile2%" (
+	echo [INFO] "%ISOFile2%" was not found
+	echo Did you name the ROM correctly and place it in the correct folder?
+	echo Exiting in 10 seconds..
+	C:\Windows\System32\timeout.exe /t 10 /nobreak >nul
+	exit /b 0
+)
 
 echo [INFO] Compiling patches for disc2
 
