4072f9655576a4e5b28fc872cfe463f926b80682
NewGBAXL <81663474+NewGBAXL@users.noreply.github.com>
2024-01-05 22:14:58 -0500

    Main, Pause, and Save Menus

--- src/common/Text.asm
+++ src/common/Text.asm
@@ -1,38 +1,32 @@
-// General Menus - 11 strings
-Text($000D5E90, "[x32][y32]             [p2][x18][y18]Scenario Clear [d1>7]/[d1>8][x24][y24][p0][n][n]") ; fill $20
-Text($000D5EF9, "  [x20][y20][d4>0]YR[d2>1]MN[d2>2]DY[d2>3]HR[d2>4]MN Play time[d2>5]HRS[d2>6]MIN[x24][y24]") ; fill $20
-//Text(, "[n]        NO DATA       ")
-//Text(, "[n]        NEW GAME      ")
-//Text(, "A Memory Card error has occurred.") ; fill 7
-Text($000D5FD8, "Page [d2>0]/[d2>1]") ; fill 1
+// General Menus - 6 strings
+Text($000D5E90, "[x32][y32]                         [p2][x18][y18]Scenario Clear [d1>7]/[d1>8][x24][y24][p0][n][n]") ; fill $20
+Text($000D5EF4, "[x20][y20]       [d2>1]/[d2>2]/[d4>0] [d2>3]:[d2>4]  Play time:[d2>5]hrs [d2>6]mins[x24][y24]") ; fill $20
+//Text($000D5EF4, "[x20][y20][d4>0]YR[d2>1]MN[d2>2]DY[d2>3]HR[d2>4]MN Play time[d2>5]HRS[d2>6]MIN[x24][y24]") ; fill $20
+//Text($000D5F54, "[n]            NO DATA") ; fill 11
+//Text($000D5F78, "[n]           NEW GAME") ; fill 12
+Text($000D5FD8, "Page [d2>0]/[d2>1]") ; fill 3
 Text($000DADC0, "Return to the title screen?") ; fill 3
-//Text($, )
-//Text($, )
-//Text($, )
-//Text($, )
-
-//todo: distinguish between JP & ENG space chars 
 
 // Chapter Names - 22 strings
-Text($000DA6C0, "         Dummy Data         "); fill $20 //Dummy
-Text($000DA6D4, "        - Prologue -        "); fill $20
-Text($000DA708, "        - Beginning -       ")
-Text($000DA73C, "        - Encounter -                           ")
-Text($000DA770, "        - Ferry Crash -                         ")
-Text($000DA7A4, "        - Miracle Save -                        ")
-Text($000DA7D8, "        - Miracle Girl -                        ")
-Text($000DA80C, "  - Legends of Sea People -                     ")
-Text($000DA840, " - 2 People on the Run -                        ")
-Text($000DA874, " - Distand Memory -                             ")
-Text($000DA8A8, " - Minupa of the Sea People -                   ")
-Text($000DA8DC, " - Theo's Descision -                           ")
-Text($000DA910, " - Hidden Research -                            ")
-Text($000DA944, " - To the Rescue -                              ")
-Text($000DA978, " - Letter from Liana -                          ")
-Text($000DA9AC, " - Abrio's Longing -                            ")
-Text($000DA9E0, " - Letter from Liana -                          ") 
-Text($000DAA14, "         Dummy Data         "); fill $20 //Dummy
-Text($000DAA48, " - Sardo the Great Power -                      ")
-Text($000DAA7C, "         Dummy Data         "); fill $20 //Dummy
-Text($000DAAB0, " - Infiltration of a Research Institute -       ")
-Text($000DAAE4, " - Bicycle Escape -                             ") 
+Text($000DA6C0, "                  Dummy Data         ") //Dummy
+Text($000DA6D4, "                 - Prologue -                   ")
+Text($000DA708, "                 - Beginning -                  ")
+Text($000DA73C, "            - A Fateful Encounter -             ")
+Text($000DA770, "                - Ferry Crash -                 ")
+Text($000DA7A4, "               - Miracle Save -                 ")
+Text($000DA7D8, "               - Miracle Girl -                 ")
+Text($000DA80C, "           - Legends of Sea People -            ")
+Text($000DA840, "           - Two People on the Run -            ")
+Text($000DA874, "              - Distant Memory -                ")
+Text($000DA8A8, "          - Minupa of the Sea People -          ")
+Text($000DA8DC, "              - Theo's Decision -               ")
+Text($000DA910, "              - Hidden Research -               ")
+Text($000DA944, "              - A Daring Rescue -               ")
+Text($000DA978, "            - A Letter from Liana -             ")
+Text($000DA9AC, "              - Abrio's Longing -               ")
+Text($000DA9E0, "            - A Letter from Liana -             ") 
+Text($000DAA14, "                  Dummy Data                    ") //Dummy
+Text($000DAA48, "           - Sardo the Great Power -            ")
+Text($000DAA7C, "                  Dummy Data                    ") //Dummy
+Text($000DAAB0, "     - Infiltration of a Research Institute -   ")
+Text($000DAAE4, "              - Bicycle Escape -                ") 
