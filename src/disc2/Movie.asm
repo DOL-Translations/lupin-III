92b39df5820157f0eb55498dbe0f93bd8d8481e9
NewGBAXL <81663474+NewGBAXL@users.noreply.github.com>
2024-06-10 13:02:42 -0400

    Save data description

--- src/disc2/Movie.asm
+++ src/disc2/Movie.asm
@@ -1,3 +1,4 @@
 // Movies
 ReplaceAsset($A2DDA4, "fs/movie/last_00.thp", 822230008)
 ReplaceAsset($31A5199C, "fs/movie/last_01.thp", 190530040)
+ReplaceAsset($3D005B94, "fs/movie/staff.thp", 55687576)
